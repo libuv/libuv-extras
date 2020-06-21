58b3cb6abdb1afabcd4b20b5b251e68a6d4e0f0e
Jiawen Geng <technicalcute@gmail.com>
2020-06-21 20:26:22 +0800

    Add android support showcase
    PR-URL: https://github.com/libuv/libuv-extras/pull/4
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

---
+++ android/libuv-demo/src/main/java/com/example/libuv_demo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.libuv_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        Button tv = findViewById(R.id.sample_text);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, stringFromJNI(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
